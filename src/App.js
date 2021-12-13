8e717a6d9429a848aae43f0d2a905f303590e37a
Frederick Katsura <katsurinstudios@protonmail.ch>
2021-12-13 15:08:14 -0400

    Created React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Item } from './itemModel';
 
 function App() {
+  const [item, setItem] = useState([]);
+  const [currTitle, setTitleInput ] = useState('');
+  const [currDesc, setDescInput ] = useState('');
+
+  const titleChange = (e) => { setTitleInput(e.currentTarget.value); }
+  const descChange = (e) => { setDescInput(e.currentTarget.value); }
+  const itemsChange = (e) => { setItem(e); }
+
+  const updateItems = (e) => {
+    e.preventDefault();
+    addItem(currTitle, currDesc);
+    setTitleInput("");
+    setDescInput("");
+  }
+
+  function removeItem(e) {
+    let copy = [...item];
+    copy.splice(e, 1);
+    itemsChange(copy);
+  } 
+
+  function addItem(title, desc) {
+    item.push(new Item(title, desc));
+    itemsChange(item);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="main-container">
+        <div class="todo-list">
+          <h2>My List</h2>
+          {
+            item.length === 0 &&
+            <div class="item">
+              <span class="no-item">Sorry, there is nothing to do!</span>
+            </div>
+          }
+          {
+            item.length >= 1 &&
+
+            item.map((value, index) => {
+              return (<div class="item" key={index}>
+                <span class="item-title"><b>{value.item_title}</b></span>
+                <span class="item-desc">{value.desc}</span>
+                <span class="delete" onClick={() => removeItem(index)}><button>X</button></span>
+              </div>)
+            })
+            
+          }
+        </div>
+        <br />
+        <div class="add-item">
+          <h3>Add a new task</h3>
+          <span class="input_label"><b>Title</b></span><input type="text" class="title_input" value={currTitle} onChange={titleChange}/>
+          <br />
+          <span class="input_label"><b>Description</b></span><input type="text" class="desc_input" value={currDesc} onChange={descChange}/>
+          <br />
+          <br />
+          <span class="add_button" onClick={updateItems}><button>Add Item</button></span>
+        </div>
+      </div>
+      </>
   );
 }
 
---
+++ src/itemModel.js
@@ -0,0 +1,9 @@
+export class Item {
+    item_title;
+    desc;
+
+    constructor(item_title, item_desc) {
+        this.item_title = item_title;
+        this.desc = item_desc;
+    }
+}
